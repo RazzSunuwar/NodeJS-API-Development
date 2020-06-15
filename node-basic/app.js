50862f1659d477e528dbd57fe3dd64a675a2e88b
raysunwar <ray@whitehatagency.com.au>
2020-06-15 07:18:01 +0545

    Writing the function

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,1 +1,8 @@
-console.log("Hello World! This is the first NodeJS File in this repository.");
+// console.log("Hello World! This is the first NodeJS File in this repository.");
+
+function sum(a, b){
+  return a + b;
+}
+
+let total = sum(10, 20);
+console.log("Total:", total);    //Total: 30
