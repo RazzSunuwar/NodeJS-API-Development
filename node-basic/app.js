1dbf9bc48deefafbf7a41c736a4347101c255e41
raysunwar <ray@whitehatagency.com.au>
2020-06-15 07:43:09 +0545

    Import/Export

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,8 +1,5 @@
-// console.log("Hello World! This is the first NodeJS File in this repository.");
-
-function sum(a, b){
-  return a + b;
-}
-
-let total = sum(10, 20);
+const helpers = require('./helpers');
+let total = helpers.sum(10, 20);
 console.log("Total:", total);    //Total: 30
+
+// console.log("Hello World! This is the first NodeJS File in this repository.");
