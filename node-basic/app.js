84e07acccc593ade4b766d993fe1bb0df8b3878b
raysunwar <ray@whitehatagency.com.au>
2020-06-15 08:00:50 +0545

    Object destructuring

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,5 +1,5 @@
-const helpers = require('./helpers');
-let total = helpers.sum(10, 20);
+const { sum } = require('./helpers');
+let total = sum(10, 20);
 console.log("Total:", total);    //Total: 30
 
 // console.log("Hello World! This is the first NodeJS File in this repository.");
