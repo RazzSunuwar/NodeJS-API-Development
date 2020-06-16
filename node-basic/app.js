8241158823d9525aa7368de5595961e56aa473a5
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-16 20:35:01 +0545

    Using node js core modules

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,5 +1,17 @@
 const { sum } = require('./helpers');
+const http = require('http');
+const port = 5000;
+const server = http.createServer((req, res) =>{
+    console.log("Request url>>>", req.url);
+    res.send('Hello World');
+    res.end();
+});
+server.listen(port, (err, done) => {
+    if(err){
+        console.log(`Error on server listening at port ${port}`);
+    } return console.log(`Success on server listening at port ${port}`);
+})
 let total = sum(10, 20);
 console.log("Total:", total);    //Total: 30
 
-// console.log("Hello World! This is the first NodeJS File in this repository.");
+console.log("Hello World! This is the first NodeJS File in this repository.");
