e5b6214667ec61a7c0530adf90cda453641e5063
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-17 22:21:16 +0545

    Using Express

---
+++ Practice-Project/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = process.env.PORT || 3001;
+const app = express();
+app.get('/', (req,res) => {
+  res.send("Hello World");
+})
+app.listen(port, (err, done) => {
+  if(err){
+    console.log(`Error on listening server at port ${port}`);
+  } return console.log(`Success on listening server at port ${port}`);
+});
