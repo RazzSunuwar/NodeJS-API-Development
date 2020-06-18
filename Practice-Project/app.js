09c84a8cb5b7d1fed76563da42263bb5051b602e
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-18 20:38:10 +0545

    Synchronous Programming

--- Practice-Project/app.js
+++ Practice-Project/app.js
@@ -10,12 +10,22 @@
 //   } return console.log(`Success on listening server at port ${port}`);
 // });
 
+// Asynchronous programming
 
 const fs = require("fs");
 const fileName = "target.txt";
-fs.readFile(fileName, 'utf-8', (err, data) => {
-  if(err) {
-    console.log(err)
-  }
-  console.log(data)
-});
+// fs.readFile(fileName, 'utf-8', (err, data) => {
+//   if(err) {
+//     console.log(err)
+//   }
+//   console.log(data)
+// });
+// console.log("NodeJS is async programming...");
+
+
+
+// Synchronous programming
+
+const data = fs.readFileSync(fileName, 'utf-8');
+console.log(data);
+console.log("NodeJS is sync programming...");
