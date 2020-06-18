6847a2d3ded364426b4bbb27b68103dea86b7560
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-18 20:17:54 +0545

    Asyncronous Programming

--- Practice-Project/app.js
+++ Practice-Project/app.js
@@ -13,4 +13,9 @@
 
 const fs = require("fs");
 const fileName = "target.txt";
-fs.watch(fileName, () => console.log(`File Changed`));
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if(err) {
+    console.log(err)
+  }
+  console.log(data)
+});
