60ebd3e3672f00d2f54baa9decf56525eb04c526
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-19 22:15:53 +0545

    Creating server with express

--- NODEJS-API/app.js
+++ NODEJS-API/app.js
@@ -3,11 +3,13 @@ const app = express();
 const port = process.env.PORT || 3005;
 
 app.get('/', (req, res) => {
-  res.send("Hello World from NODEJS")
-});
+    res.send("Hello World from NODEJS")
+  }
+);
 
 app.listen(port, (err, done) => {
-  if(err){
-    console.log(`Error listening at port ${port}`);
-  } return console.log(`Success listening at port ${port}`)
-});
+    if(err){
+      console.log(`Error listening at port ${port}`);
+    } return console.log(`Success listening at port ${port}`)
+  }
+);
